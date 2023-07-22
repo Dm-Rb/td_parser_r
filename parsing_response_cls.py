2ebcb645c76b337c3aad96c5660a84f775c464e3
Dm-Rb <dm.rb.418http@gmail.com>
2023-07-22 19:15:00 +0300

    переписан класс RequestToAPI, все методы classmethod, добавлена опция поиска отсутствующего в БД авто

--- parsing_response_cls.py
+++ parsing_response_cls.py
@@ -35,7 +35,7 @@ class Parsing:
 
         with self.connection:
             return self.cursor.execute(
-                "INSERT INTO 'cars' ('tecdoc_id', 'tecdoc_id') VALUES(?, ?)",
+                "INSERT INTO 'cars' ('tecdoc_id', 'data') VALUES(?, ?)",
                 (tecdoc_id, data_str,))
 
     # Разбор ответа json
